c723160ddca996c0bf5a65ef8370241f8d83cef2
jpelcis <jpelcis@499bc0f0-4df5-0310-a575-b830949ceb61>
2006-07-16 22:53:19 +0000

    Second try to add the files before I switch SVN clients.
    git-svn-id: http://svn.dsource.org/projects/ddbi/trunk@24 499bc0f0-4df5-0310-a575-b830949ceb61

---
+++ dbi/msql/all.d
@@ -0,0 +1,16 @@
+/**
+ * Authors: The D DBI project
+ *
+ * Version: 0.2.2
+ *
+ * Copyright: BSD license
+ */
+module dbi.msql.all;
+
+version (build) {
+	pragma (ignore);
+}
+
+public import	dbi.msql.MsqlDatabase,
+		dbi.msql.MsqlResult,
+		dbi.all;
---
+++ dbi/oracle/all.d
@@ -0,0 +1,16 @@
+/**
+ * Authors: The D DBI project
+ *
+ * Version: 0.2.2
+ *
+ * Copyright: BSD license
+ */
+module dbi.oracle.all;
+
+version (build) {
+	pragma (ignore);
+}
+
+public import	dbi.oracle.OracleDatabase,
+		dbi.oracle.OracleResult,
+		dbi.all;
