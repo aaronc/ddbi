9579cdb7b019ceaaff0e39ef22e0eba87e626318
stonecobra <stonecobra@499bc0f0-4df5-0310-a575-b830949ceb61>
2007-09-02 22:24:53 +0000

    added registry and beginning of URL based database access
    git-svn-id: http://svn.dsource.org/projects/ddbi/trunk@65 499bc0f0-4df5-0310-a575-b830949ceb61

---
+++ dbi/Registry.d
@@ -0,0 +1,56 @@
+module dbi.Registry;
+
+private import dbi.Database;
+
+private import tango.text.Util;
+private import tango.util.log.Log;
+
+private static Registerable[char[]] dbs;
+private static Logger logger;
+
+static this() {
+	logger = Log.getLogger("dbi.Registry");
+}
+
+/**
+ * Interface for registering a database provider.  This will allow you to only 
+ * link the database drivers that you will use.
+ */
+public interface Registerable {
+	public char[] getPrefix();
+	public Database getInstance(char[] url);
+}
+
+/**
+ * Used by each database provider to 'register' itself.  This registration is 
+ * usually done via a static constructor, so all you need to do is import the 
+ * database that you want to use.
+ * 
+ * Params:
+ *     newDB = Registerable instance that will create Database instances when 
+ *     asked via getDatabaseForURL.
+ */
+public static void registerDatabase(Registerable newDB) {
+	logger.trace("registering database: " ~ newDB.getPrefix());
+	dbs[newDB.getPrefix()] = newDB;
+}
+
+/**
+ * Given a database URL, instaniate and return a Database instance.
+ * 
+ * A database URL looks like:
+ *   dbprefix://params
+ * Where dbprefix is the database provider, such as mysql or oracle.
+ * params is currently implementation defined.
+ * 
+ * Params:
+ *     dbUrl = The URL of the database you wish to connect to.
+ * Returns: A fully instantiated Database instance.  TODO: Should it call connect()?
+ */
+public static Database getDatabaseForURL(char[] dbUrl) {
+	char[] origURL = dbUrl.dup;
+	logger.trace("getDatabaseForURL: " ~ dbUrl);
+	char[][] fields = delimit(dbUrl, ":");
+	char[] prefix = fields[0];
+	return dbs[prefix].getInstance(dbUrl[prefix.length+3 .. $]);
+}
