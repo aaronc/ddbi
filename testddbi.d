8eabd4de1869ad26c84747dacbc558f963fa1972
stonecobra <stonecobra@499bc0f0-4df5-0310-a575-b830949ceb61>
2007-12-27 23:28:22 +0000

    add new test client program
    git-svn-id: http://svn.dsource.org/projects/ddbi/trunk@78 499bc0f0-4df5-0310-a575-b830949ceb61

---
+++ testddbi.d
@@ -0,0 +1,34 @@
+module testddbi;
+
+import tango.util.log.Log,
+       tango.util.log.Configurator;
+
+import dbi.Registry, 
+       dbi.Database, 
+       dbi.Statement;
+
+version (dbi_sqlite) {
+    //This import is to fire the static constructor that registers Sqlite with the Registry
+	private import dbi.sqlite.SqliteDatabase;
+    //TODO: how to make this fire in the future, without changing code?  Perhaps just import all supported?
+}
+
+version (dbi_mysql) {
+    //This import is to fire the static constructor that registers Sqlite with the Registry
+	private import dbi.mysql.MysqlDatabase;
+}
+
+void main(char[][] args) {
+	auto logger = Log.getLogger(args[0]);
+	logger.info("testing ddbi");
+
+	if (args.length > 1) {
+		auto db = getDatabaseForURL(args[1]);
+		
+	} else {
+		logger.error("usage: testddbi DBURL");
+	}
+	
+	logger.info("testing complete");
+}
+
